b071bc1f5e6502904c511866fe6c7c365ef301da
Yaswant Narayan <yoyaswant@gmail.com>
2018-06-24 17:16:04 +0530

    add location listener

--- AndroidCompanion/app/src/main/java/com/dscvit/smartglasscompanion/MainActivity.kt
+++ AndroidCompanion/app/src/main/java/com/dscvit/smartglasscompanion/MainActivity.kt
@@ -1,12 +1,59 @@
 package com.dscvit.smartglasscompanion
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
+
+    lateinit var locationManager: LocationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(applicationContext, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(applicationContext, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION), 101)
+        }
+
+        getLocationBtn.setOnClickListener {
+            getLocation()
+        }
+    }
+
+    fun getLocation() {
+        try {
+            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 5f, this)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        locationText.text = "Latitude: ${location?.getLatitude()}\n Longitude: ${location?.getLongitude()}"
+        Log.d("LOCATION", "Latitude: ${location?.getLatitude()}\n Longitude: ${location?.getLongitude()}")
+    }
+
+    override fun onProviderDisabled(p0: String?) {
+        Toast.makeText(this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+
+    }
+
+    override fun onProviderEnabled(p0: String?) {
+
     }
 }
