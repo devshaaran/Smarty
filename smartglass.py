5433bf059942f49ad4f0dd5664c1eb39b5d41498
shaaran Lakshminarayanan <34472894+devshaaran@users.noreply.github.com>
2018-06-27 13:41:33 +0530

    Added location commit
    reduced vulnerability to location

--- smartglass.py
+++ smartglass.py
@@ -4,7 +4,6 @@ from PIL import Image, ImageSequence
 from luma.core.sprite_system import framerate_regulator
 import RPi.GPIO as GPIO
 import picamera
-import time
 import random as rand
 import io
 import sys
@@ -12,6 +11,12 @@ import face_recognition
 import numpy as np
 from luma.core.virtual import viewport
 from luma.core.render import canvas
+import urllib.request, json
+from bs4 import BeautifulSoup
+from nltk import word_tokenize
+from time import sleep
+from firebase import firebase
+
 
 
 cn1 = 7
@@ -86,7 +91,7 @@ def camera_inititate():
                 photo.save('home/'+ str(rand.randint(0,99999999))+'.jpg')
                 # display on screen for a few seconds
                 device.display(photo.convert(device.mode))
-                time.sleep(displayTime)
+                sleep(displayTime)
             if (GPIO.input(cn3) == 1):
                 camera.close()
                 break
@@ -142,10 +147,137 @@ def findwho_initiate():
             if (GPIO.input(cn3) == 1):
                 break
 
+def location_smart():
+    from firebase import firebase
+    # Google MapsDdirections API endpoint
+    endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
+    api_key = 'AIzaSyAu42C9VNekg6jhnEIg4dipqwmWowRHlCM'
+    firebase = firebase.FirebaseApplication('https://smartglass-e01ec.firebaseio.com/', None)
+    virtual = viewport(device, width=device.width, height=768)
+
+    for _ in range(2):
+        with canvas(virtual) as draw:
+            draw.text((0, 12), 'Are you sure you have set the destination in the app?', fill="white")
+
+    while (GPIO.input(cn2) == 1):
+        break
+
+
+    while True:
+        whole_direct = firebase.get('/directions', None)
+        latt = whole_direct['lattitude']
+        long = whole_direct['longitude']
+        latt_desti = whole_direct['desti_latitude']
+        long_desti = whole_direct['desti_longitude']
+        destination = (str(latt_desti) + ', ' + str(long_desti))
+        start_destin = (str(latt)+', '+str(long))
+        # Asks the user to input Where they are and where they want to go.
+        origin = start_destin.replace(' ', '+')
+        # Building the URL for the request
+        nav_request = 'origin={}&destination={}&key={}'.format(origin, destination, api_key)
+        request = endpoint + nav_request
+        # Sends the request and reads the response.
+        response = urllib.request.urlopen(request).read()
+        # Loads response as JSON
+        directions = json.loads(response)
+        routes = directions['routes']
+        legs = routes[0]['legs']
+        # print(len(legs))
+        first_step = legs[0]['steps']
+        # print(len(first_step))
+        instructions = first_step[0]['html_instructions']
+        if len(first_step) > 1:
+            instructions_1 = first_step[1]['html_instructions']
+            soup_1 = BeautifulSoup(instructions_1, "html5lib")
+            go_direction_1 = soup_1.get_text()
+
+        distance = first_step[0]['distance']['text']
+        soup = BeautifulSoup(instructions, "html5lib")
+        go_direction = soup.get_text()
+        conversion = distance.split(' ')
+        org_distance = 0
+        if conversion[1] == 'km':
+            org_distance = float(conversion[0]) * 1000
+        if conversion[1] == 'm':
+            org_distance = conversion[0]
+
+        collector = word_tokenize(go_direction)
+        # print(distance)
+        # print(int(org_distance))
+
+        main_way_list = go_direction.split('(')
+        main_way = main_way_list[0]
+
+        directions = ['north', 'northeast', 'northwest', 'straight', 'left', 'right']
+
+        if int(org_distance) < 6:
+            goway = []
+
+            for d in main_way:
+                for e in directions:
+                    if d.lower() == e:
+                        goway.append(d)
+
+            if len(goway) == 0:
+                if len(first_step) > 1:
+                    print(go_direction_1)
+                else:
+                    print('go on you are on your last leg')
+                print('turn in ' + str(org_distance) + ' m')
+
+            else:
+                if len(first_step) > 1:
+                    toggle_direction = goway[1]
+                    print(toggle_direction)
+                    if toggle_direction == 'north' or toggle_direction == 'straight':
+                        straighter = Image.open(img_path_straight)
+                        device.display(straighter.convert(device.mode))
+                    if toggle_direction == 'northeast':
+                        straighter = Image.open(img_path_northeast)
+                        device.display(straighter.convert(device.mode))
+                    if toggle_direction == 'northwest':
+                        straighter = Image.open(img_path_northwest)
+                        device.display(straighter.convert(device.mode))
+                    if toggle_direction == 'right':
+                        straighter = Image.open(img_path_right)
+                        device.display(straighter.convert(device.mode))
+                    if toggle_direction == 'left':
+                        straighter = Image.open(img_path_left)
+                        device.display(straighter.convert(device.mode))
+
+                else:
+                    print('you are on your last leg of the journey')
+                print('turn in ' + str(org_distance) + ' m')
+
+
+        else:
+            goway = []
+
+            for d in main_way:
+                for e in directions:
+                    if d.lower() == e:
+                        goway.append(d)
+
+            if len(goway) == 0:
+                print(go_direction)
+                straighter = Image.open(img_path_straight)
+                device.display(straighter.convert(device.mode))
+                print('go on untill ' + str(org_distance) + ' metres')
+
+            else:
+                toggle_direction = goway[0]
+                print(toggle_direction)
+                straighter = Image.open(img_path_straight)
+                device.display(straighter.convert(device.mode))
+                print('go on untill ' + str(org_distance) + ' metres')
+
+            sleep(0.2)
+
 
 def main():
+
     try:
-        while (True):
+        while True:
             button_next = GPIO.input(cn1)
             button_ok = GPIO.input(cn2)
             button_back = GPIO.input(cn3)
@@ -159,16 +291,15 @@ def main():
             if opener == True:
                 findwho_initiate()
                 opener = False
+            initiate_gif('D:\Youtube\linkedin\smart glasses\\anim_gif\\location_gif')
+            show_image('D:\Youtube\linkedin\smart glasses\\location')
+            if opener == True:
+                location_smart()
+                opener = False
 
             #TODO bring in location and list
 
-
-
-
-
-
-
-            time.sleep(1)
+            sleep(1)
     except KeyboardInterrupt:
         GPIO.cleanup()
 
