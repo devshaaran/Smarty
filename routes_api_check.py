eeb84b3d50fa6ad5aa895fee13547cb87a27319c
shaaran Lakshminarayanan <34472894+devshaaran@users.noreply.github.com>
2018-06-28 18:55:54 +0530

    Update major
    major additons

--- routes_api_check.py
+++ routes_api_check.py
@@ -6,7 +6,7 @@ from time import sleep
 def mutual(start_destin,path_to):
     # Google MapsDdirections API endpoint
     endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
-    api_key = 'Your API key '
+    api_key = 'AIzaSyAu42C9VNekg6jhnEIg4dipqwmWowRHlCM'
     # Asks the user to input Where they are and where they want to go.
     origin = start_destin.replace(' ', '+')
     destination = path_to.replace(' ', '+')
@@ -67,7 +67,7 @@ def mutual(start_destin,path_to):
 def location_initiate(start_destin,path_to):
     # Google MapsDdirections API endpoint
     endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
-    api_key = 'Your API key'
+    api_key = 'AIzaSyAu42C9VNekg6jhnEIg4dipqwmWowRHlCM'
     destination = path_to.replace(' ', '+')
 
     while True:
@@ -152,7 +152,117 @@ def location_initiate(start_destin,path_to):
 
         sleep(1)
 
-fromo  = '12.976252, 80.220971'
-tooo = '12.968829, 80.216497'
+def location_smart():
+    from firebase import firebase
+    # Google Maps Ddirections API endpoint
+    endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
+    api_key = 'AIzaSyAu42C9VNekg6jhnEIg4dipqwmWowRHlCM'
+    firebase = firebase.FirebaseApplication('https://smartglass-e01ec.firebaseio.com/', None)
 
-location_initiate('12.976252, 80.220971','12.968829, 80.216497')
+    while True:
+
+
+
+            whole_direct = firebase.get('/directions', None)
+            latt = whole_direct['latitude']
+            long = whole_direct['longitude']
+            latt_desti = whole_direct['desti_latitude']
+            long_desti = whole_direct['desti_longitude']
+            print(latt)
+            print(long)
+            destination = (str(latt_desti) + ', ' + str(long_desti))
+            start_destin = (str(latt) + ', ' + str(long))
+            # Asks the user to input Where they are and where they want to go.
+            origin = start_destin.replace(' ', '+')
+            destination = destination.replace(' ', '+')
+            # Building the URL for the request
+            nav_request = 'origin={}&destination={}&key={}'.format(origin, destination, api_key)
+            request = endpoint + nav_request
+            # Sends the request and reads the response.
+            response = urllib.request.urlopen(request).read()
+            # Loads response as JSON
+            directions = json.loads(response)
+            routes = directions['routes']
+            legs = routes[0]['legs']
+            # print(len(legs))
+            first_step = legs[0]['steps']
+            print(len(first_step))
+
+            if len(first_step) > 1 :
+                instructions_1 = first_step[1]['html_instructions']
+                soup_1 = BeautifulSoup(instructions_1, "html5lib")
+                go_direction_1 = soup_1.get_text()
+
+            instructions = first_step[0]['html_instructions']
+            distance = first_step[0]['distance']['text']
+            soup = BeautifulSoup(instructions, "html5lib")
+            go_direction = soup.get_text()
+            conversion = distance.split(' ')
+            org_distance = 0
+            if conversion[1] == 'km':
+                org_distance = float(conversion[0]) * 1000
+            if conversion[1] == 'm':
+                org_distance = conversion[0]
+
+            collector = word_tokenize(go_direction)
+            # print(distance)
+            # print(int(org_distance))
+
+            main_way_list = go_direction.split('(')
+            main_way = main_way_list[0]
+
+            directions = ['north', 'northeast', 'northwest', 'straight', 'left', 'right', 'south']
+
+            if int(org_distance) < 10:
+                goway = []
+
+                for d in main_way:
+                    for e in directions:
+                        if d.lower() == e:
+                            goway.append(d)
+
+                if len(goway) == 0:
+                    if len(first_step) > 1:
+                        print(go_direction_1)
+                    else:
+                        print('go on you are on your last leg')
+                    print('turn in ' + str(org_distance) + ' m')
+
+                else:
+                    if len(first_step) > 1:
+                        toggle_direction = goway[1]
+                        print(toggle_direction)
+                    else:
+                        print(toggle_direction)
+                        print('go on you are on your last leg')
+                    print('turn in ' + str(org_distance) + ' m')
+
+
+            else:
+                goway = []
+
+                for d in main_way:
+                    for e in directions:
+                        if d.lower() == e:
+                            goway.append(d)
+
+                if len(goway) == 0:
+                    print(go_direction)
+                    print('straight')
+                    print('go on untill ' + str(org_distance) + ' metres')
+
+                else:
+                    toggle_direction = goway[0]
+                    if toggle_direction == 'south':
+                        print('go back')
+                    else:
+                        print(toggle_direction)
+                        print(go_direction)
+                        print('straight')
+
+                    print('go on untill ' + str(org_distance) + ' metres')
+
+
+location_smart()
+
+
